dd71315a0f390d8fa6c63991e9036dd9933b5c98
Ran You <ran.you8311@gmail.com>
2021-04-12 20:40:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3 className='main-title'>Tic-Tac-Toe</h3>
+      <div className='result-container'>
+        <div className='result-player'>
+          <h3>Player 1</h3>
+          <h3>Score:0</h3>
+        </div>
+        <div className='result-notice'>
+          <h4>Player 1 wins!</h4>
+        </div>
+        <div className='result-player'>
+          <h3>Player 2</h3>
+          <h3>Score:0</h3>
+        </div>
+      </div>
+      <div className='ttt-board'>
+        {array.map((item) => {
+          return <button className='item'>{item}</button>;
+        })}
+      </div>
+      <div className='game-container'>
+        <button className='new-g-btn'> New Round</button>
+        <button className='new-g-btn'> New Game </button>
+      </div>
     </div>
   );
 }
